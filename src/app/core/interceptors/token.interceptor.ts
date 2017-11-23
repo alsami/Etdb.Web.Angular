9bf676827926ed6bff275b489ddefda137009b91
SamiAl90 <al.khatib.sami1990@gmail.com>
2017-11-23 23:38:17 +0100

    fix: TokenInterceptor for JIT builds

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { TokenStorageService } from '../services/token-storage.service';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
     public constructor(private tokenStorageService: TokenStorageService) {}
