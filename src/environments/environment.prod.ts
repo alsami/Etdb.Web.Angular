d03fca88eebe84423612f4f331fdf51c9534181e
SamiAl90 <al.khatib.sami1990@gmail.com>
2017-11-26 21:26:18 +0100

    fix environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
+  userserviceUrl: 'http://localhost:5000/api/v1/',
   webserviceMainUrl: 'http://localhost:5001/api/main/v1/',
   webserviceAdminUrl: 'http://localhost:5001/api/admin/v1/',
-  userserviceMainUrl: 'http://localhost:5000/api/main/v1/',
-  userserviceAdminUrl: 'http://localhost:5000/api/admin/v1/',
   userserviceAuthUrl: 'http://localhost:5000/connect/token',
   userserviceAuthProfileUrl: 'http://localhost:5000/connect/userinfo',
   userserviceConfigUrl: 'http://localhost:5000/api/config/'
