9454e48e2def17af10788f0ac9743da85637995e
SamiAl90 <al.khatib.sami1990@gmail.com>
2017-11-26 22:48:32 +0100

    .

--- src/app/core/models/event-source-success.response.model.ts
+++ src/app/core/models/event-source-success.response.model.ts
@@ -1,6 +1,5 @@
 import { EventSourceResponse } from '@app/core/models/event-source.response.model';
 
 export interface EventSourceSuccessResponse extends EventSourceResponse {
-    success: boolean;
     data: any;
 }
