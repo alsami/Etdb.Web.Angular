97290a1c5932d551f0f83ec426f8141d378219ca
Birdperson90 <al.khatib.sami1990@gmail.com>
2017-12-17 12:48:01 +0100

    remove function

--- src/app/abstractions/api.service.ts
+++ src/app/abstractions/api.service.ts
@@ -1,15 +1,9 @@
 import {
     HttpHeaders
 } from '@angular/common/http';
-import { IdentityToken } from '../core/models/identity-token.model';
 
 export abstract class ApiService {
     protected baseHeaders: HttpHeaders = new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('charset', 'utf-8');
-
-    protected setAuthorizationHeader(headers: HttpHeaders): HttpHeaders {
-        const token: IdentityToken = JSON.parse(window.localStorage.getItem('token'));
-        return headers.set('Authorization', 'Bearer ' + token.access_token);
-    }
 }
