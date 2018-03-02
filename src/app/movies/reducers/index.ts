2d0b0e8ac1015556e594069da9f2e1324872be89
BirdPerson90 <al.khatib.sami@gmail.com>
2018-03-02 17:38:30 +0100

    .

--- src/app/movies/reducers/index.ts
+++ src/app/movies/reducers/index.ts
@@ -20,37 +20,8 @@ export const reducers = {
     collection: fromCollection.reducer,
 };
 
-/**
- * A selector function is a map function factory. We pass it parameters and it
- * returns a function that maps from the larger state tree into a smaller
- * piece of state. This selector simply selects the `movies` state.
- *
- * Selectors are used with the `select` operator.
- *
- * ```ts
- * class MyComponent {
- * 	constructor(state$: Observable<State>) {
- * 	  this.booksState$ = state$.select(getBooksState);
- * 	}
- * }
- * ```
- */
-
-/**
- * The createFeatureSelector function selects a piece of state from the root of the state object.
- * This is used for selecting feature states that are loaded eagerly or lazily.
-*/
 export const getMoviesState = createFeatureSelector<MoviesState>('movies');
 
-/**
- * Every reducer module exports selector functions, however child reducers
- * have no knowledge of the overall state tree. To make them useable, we
- * need to make new selectors that wrap them.
- *
- * The createSelector function creates very efficient selectors that are memoized and
- * only recompute when arguments change. The created selectors can also be composed
- * together to select different pieces of state.
- */
 export const getMovieEntitiesState = createSelector(
     getMoviesState,
     state => state.movies
@@ -61,14 +32,6 @@ export const getSelectedMovieId = createSelector(
     fromMovies.getSelectedId
 );
 
-/**
- * Adapters created with @ngrx/entity generate
- * commonly used selector functions including
- * getting all ids in the record set, a dictionary
- * of the records by id, an array of records and
- * the total number of records. This reducers boilerplate
- * in selecting records from the entity state.
- */
 export const {
     selectIds: getMovieIds,
     selectEntities: getMovieEntities,
@@ -84,10 +47,6 @@ export const getSelectedMovie = createSelector(
     }
 );
 
-/**
- * Just like with the movies selectors, we also have to compose the search
- * reducer's and collection reducer's selectors.
- */
 export const getSearchState = createSelector(
     getMoviesState,
     (state: MoviesState) => state.search
@@ -106,10 +65,6 @@ export const getSearchLoading = createSelector(
     fromSearch.getLoading
 );
 
-/**
- * Some selector functions create joins across parts of state. This selector
- * composes the search result IDs to return an array of movies in the store.
- */
 export const getSearchResults = createSelector(
     getMovieEntities,
     getSearchMovieIds,
