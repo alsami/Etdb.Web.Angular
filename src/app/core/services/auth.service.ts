f45f0921c34c531da2b7fe275487652d1df426db
Birdperson90 <al.khatib.sami1990@gmail.com>
2018-04-27 22:20:00 +0200

    fix: scopes for authentication

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -8,6 +8,11 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthService extends ApiService {
+    private headers: HttpHeaders = new HttpHeaders()
+        .set('Authorization', 'Basic d2ViLmNsaWVudDpnaXg4ZXhrbmY5c3h2bmh2dmtyZ3R6bTVlYjdvNHU3bWtwbW5xODZtbzkwd2plNDlxdQ==')
+        .set('Accept', 'application/json')
+        .set('cache-control', 'no-cache');
+
     public constructor(private http: HttpClient) {
         super();
     }
@@ -15,54 +20,38 @@ export class AuthService extends ApiService {
     public loginViaCredentials(login: UserLogin): Observable<IdentityToken> {
         const formData = new FormData();
 
-        const headers = new HttpHeaders()
-            .set('Authorization', 'Basic d2ViLmNsaWVudDpnaXg4ZXhrbmY5c3h2bmh2dmtyZ3R6bTVlYjdvNHU3bWtwbW5xODZtbzkwd2plNDlxdQ==')
-            .set('Accept', 'application/json')
-            .set('cache-control', 'no-cache');
-
         formData.append('grant_type', 'password');
         formData.append('username', login.userName);
         formData.append('password', login.password);
-        formData.append('scope', 'UserService WebService openid offline_access');
+        formData.append('scope', 'UserService FileService IndexService StorageService openid offline_access');
 
-        return this.http.post(environment.userserviceAuthUrl, formData, {
-            headers: headers
-        }).pipe(
-            map((res: IdentityToken) => res)
-        );
+        return this.http.post<IdentityToken>(environment.userserviceAuthUrl, formData, {
+            headers: this.headers
+        });
     }
 
     public loginViaRefreshtoken(token: IdentityToken): Observable<IdentityToken> {
         const formData = new FormData();
 
-        const headers = new HttpHeaders()
-            .set('Authorization', 'Basic d2ViLmNsaWVudDpnaXg4ZXhrbmY5c3h2bmh2dmtyZ3R6bTVlYjdvNHU3bWtwbW5xODZtbzkwd2plNDlxdQ==')
-            .set('Accept', 'application/json')
-            .set('cache-control', 'no-cache');
-
         formData.append('grant_type', 'refresh_token');
         formData.append('refresh_token', token.refresh_token);
 
-        return this.http.post(environment.userserviceAuthUrl, formData, {
-            headers: headers
-        }).pipe(
-            map((res: IdentityToken) => res)
-        );
+        return this.http.post<IdentityToken>(environment.userserviceAuthUrl, formData, {
+            headers: this.headers
+        });
     }
 
     public loadIdentityUser(): Observable<IdentityUser> {
         const headers = this.baseHeaders;
 
-        return this.http.get(environment.userserviceAuthProfileUrl, {
+        return this.http.get<IdentityUser>(environment.userserviceAuthProfileUrl, {
             headers: headers
-        }).pipe(
-            map((res: IdentityUser) => res)
-        );
+        })
     }
 
     public register(registerUser: RegisterUser): Observable<any> {
         const headers = this.baseHeaders;
-        return this.http.post(environment.userserviceUrl + 'users/registration',
+        return this.http.post(environment.userserviceUrl + 'auth/registration',
             JSON.stringify(registerUser), {
                 headers: headers
             });
