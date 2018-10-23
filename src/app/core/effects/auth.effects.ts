9042ed29224bb42388088c541ec7e4698df158ed
alsami <al.khatib.sami1990@gmail.com>
2018-10-23 08:33:31 +0200

    feat: angular 7 and fix typescript related bug in auth-effects

--- src/app/core/effects/auth.effects.ts
+++ src/app/core/effects/auth.effects.ts
@@ -10,8 +10,8 @@ import { catchError, map, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthEffects {
-
-    @Effect() login$: Observable<Action> = this.actions$.pipe(
+    @Effect()
+    login$: Observable<Action> = this.actions$.pipe(
         ofType(AuthActionTypes.Login, AuthActionTypes.Registered),
         switchMap((action: authActions.Login | authActions.Registered) =>
             this.authService.loginViaCredentials(action.login).pipe(
@@ -19,11 +19,15 @@ export class AuthEffects {
                     this.tokenStorageService.storeToken(token);
                     return new authActions.LoggedIn(token, true);
                 }),
-                catchError((error: Error) => of(new authActions.LoginFailed(error)))
-            ))
+                catchError((error: Error) =>
+                    of(new authActions.LoginFailed(error))
+                )
+            )
+        )
     );
 
-    @Effect() loggedIn$: Observable<Action> = this.actions$.pipe(
+    @Effect()
+    loggedIn$: Observable<Action> = this.actions$.pipe(
         ofType(AuthActionTypes.LoggedIn),
         switchMap((action: authActions.LoggedIn) => {
             if (action.navigateToRoot) {
@@ -32,55 +36,85 @@ export class AuthEffects {
             return of();
         })
     );
-    @Effect() logout$: Observable<Action> = this.actions$.pipe(
+    @Effect()
+    logout$: Observable<Action> = this.actions$.pipe(
         ofType(AuthActionTypes.Logout),
         switchMap(() => {
             this.tokenStorageService.clearToken();
             return of();
-        }));
-
-    @Effect() register$: Observable<Action> = this.actions$.pipe(
-        ofType(AuthActionTypes.Register),
-        switchMap((action: authActions.Register) =>
-            this.authService.register(action.registerUser).pipe(
-                map(() => new authActions.Registered({
-                    userName: action.registerUser.userName,
-                    password: action.registerUser.password
-                })),
-                catchError((error: Error) => of(new authActions.RegisterFailed(error)))
-            ))
-    );
-
-    @Effect() identityUserLoad$: Observable<Action> = this.actions$.pipe(
-        ofType(AuthActionTypes.LoggedIn),
-        switchMap(() => this.authService.loadIdentityUser().pipe(
-            map(identityUser => new authActions.IdentityUserLoaded(identityUser)),
-            catchError((error: Error) => of(new authActions.IdentityUserLoadFailed(error))))
-        )
-    );
-
-    @Effect() restoreLogin$: Observable<Action> = this.actions$.pipe(
-        ofType(AuthActionTypes.RestoreLogin),
-        switchMap(() => {
-            if (!this.tokenStorageService.canRestore()) {
-                return of(new authActions.RestoreCompleted());
-            }
-            return this.authService.loginViaRefreshtoken(this.tokenStorageService.restoreToken())
-                .pipe(
-                    map(token => {
-                        this.tokenStorageService.clearToken();
-                        this.tokenStorageService.storeToken(token);
-                        return new authActions.LoggedIn(token);
-                    }),
-                    catchError((error: Error) => {
-                        this.tokenStorageService.clearToken();
-                        return of(new authActions.LoginFailed(error));
-                    })
-                );
         })
     );
 
-    public constructor(private authService: AuthService,
+    @Effect()
+    register$: Observable<Action> = this.actions$.pipe(
+        ofType(AuthActionTypes.Register),
+        switchMap((action: authActions.Register) =>
+            this.authService.register(action.registerUser).pipe(
+                map(
+                    () =>
+                        new authActions.Registered({
+                            userName: action.registerUser.userName,
+                            password: action.registerUser.password
+                        })
+                ),
+                catchError((error: Error) =>
+                    of(new authActions.RegisterFailed(error))
+                )
+            )
+        )
+    );
+
+    @Effect()
+    identityUserLoad$: Observable<Action> = this.actions$.pipe(
+        ofType(AuthActionTypes.LoggedIn),
+        switchMap(() =>
+            this.authService.loadIdentityUser().pipe(
+                map(
+                    identityUser =>
+                        new authActions.IdentityUserLoaded(identityUser)
+                ),
+                catchError((error: Error) =>
+                    of(new authActions.IdentityUserLoadFailed(error))
+                )
+            )
+        )
+    );
+
+    @Effect()
+    restoreLogin$: Observable<Action> = this.actions$.pipe(
+        ofType(AuthActionTypes.RestoreLogin),
+        switchMap(
+            (): Observable<
+                | authActions.RestoreCompleted
+                | authActions.LoggedIn
+                | authActions.LoginFailed
+            > => {
+                if (!this.tokenStorageService.canRestore()) {
+                    return of(new authActions.RestoreCompleted());
+                }
+                return this.authService
+                    .loginViaRefreshtoken(
+                        this.tokenStorageService.restoreToken()
+                    )
+                    .pipe(
+                        map(token => {
+                            this.tokenStorageService.clearToken();
+                            this.tokenStorageService.storeToken(token);
+                            return new authActions.LoggedIn(token);
+                        }),
+                        catchError((error: Error) => {
+                            this.tokenStorageService.clearToken();
+                            return of(new authActions.LoginFailed(error));
+                        })
+                    );
+            }
+        )
+    );
+
+    public constructor(
+        private authService: AuthService,
         private tokenStorageService: TokenStorageService,
-        private actions$: Actions, private router: Router) { }
+        private actions$: Actions,
+        private router: Router
+    ) {}
 }
