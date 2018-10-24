fbb7a232aff5b986d7ff84fbe4d16f219e9b803c
alsami <al.khatib.sami1990@gmail.com>
2018-10-24 15:07:58 +0200

    ui changes

--- src/app/core/components/login-form.component.ts
+++ src/app/core/components/login-form.component.ts
@@ -1,21 +1,26 @@
-import { Component, Output, EventEmitter, ViewChild, ElementRef, ChangeDetectionStrategy } from '@angular/core';
+import {
+    Component,
+    Output,
+    EventEmitter,
+    ViewChild,
+    ElementRef,
+    ChangeDetectionStrategy
+} from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { UserLogin } from '@etdb/core/models';
 
 @Component({
     selector: 'etdb-login-form',
     templateUrl: 'login-form.component.html',
-    styleUrls: [
-        'login-form.component.scss'
-    ],
+    styleUrls: ['login-form.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-
 export class LoginFormComponent {
-    @Output() requestLogin: EventEmitter<UserLogin> =
-        new EventEmitter<UserLogin>();
+    @Output()
+    requestLogin: EventEmitter<UserLogin> = new EventEmitter<UserLogin>();
 
-    @ViewChild('passwordInput') passwordInput: ElementRef;
+    @ViewChild('passwordInput')
+    passwordInput: ElementRef;
 
     loginForm: FormGroup;
 
@@ -27,29 +32,25 @@ export class LoginFormComponent {
         return this.loginForm.valid;
     }
 
-    public logInput(): void {
-        console.log(this.passwordInput);
-    }
-
     public changePasswordVisibility(): void {
         this.passwordInput.nativeElement.type === 'password'
-            ? this.passwordInput.nativeElement.type = 'text'
-            : this.passwordInput.nativeElement.type = 'password';
+            ? (this.passwordInput.nativeElement.type = 'text')
+            : (this.passwordInput.nativeElement.type = 'password');
     }
 
     public submit(): void {
-        const userLogin: UserLogin = Object.assign({}, this.loginForm.value);
+        if (!this.isFormValid()) {
+            return;
+        }
+
+        const userLogin: UserLogin = this.loginForm.value;
         this.requestLogin.emit(userLogin);
     }
 
     private buildForm(): void {
         this.loginForm = this.formBuilder.group({
-            userName: [
-                '', Validators.required
-            ],
-            password:  [
-                '', Validators.required
-            ]
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
         });
     }
 }
