5a51c1a472f642900e114e40961bea42e5137aab
alsami <al.khatib.sami1990@gmail.com>
2019-06-16 17:11:32 +0200

    chore(config): add local configuration

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUrls: {
+        userService: 'http://firebirdy-dev:8080/api/v1/'
+    },
+    etdbClientId: 'ZXRkYndlYmNsaWVudDphbmd1bGFyLmNsaWVudA==',
+    googleClientId: '545313014879-n64aegcj4rrufptpk9vso3pfs0572ae9.apps.googleusercontent.com'
+};
