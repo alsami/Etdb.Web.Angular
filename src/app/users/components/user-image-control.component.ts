15e6a0dc09a71396b2dfabe44aea32444066a48f
alsami <al.khatib.sami1990@gmail.com>
2019-07-08 20:09:56 +0200

    fix: properly recalculate tiles and delete the correct image by selecting it from the tile subject

--- src/app/users/components/user-image-control.component.ts
+++ src/app/users/components/user-image-control.component.ts
@@ -6,27 +6,27 @@ import {
     OnChanges,
     SimpleChanges,
     ViewChild,
-    ElementRef
+    ElementRef,
+    ChangeDetectionStrategy,
+    AfterViewChecked,
+    OnDestroy,
 } from '@angular/core';
-import { User, ProfileImageMetaInfo } from '@etdb/models';
-import { BehaviorSubject } from 'rxjs';
-
-interface ImageTile {
-    url: string;
-    cols: number;
-    rows: number;
-}
+import { ProfileImageMetaInfo } from '@etdb/models';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { environment } from 'environments/environment';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { delay } from 'rxjs/operators';
 
 @Component({
     selector: 'etdb-user-image-control',
     templateUrl: 'user-image-control.component.html',
-    styleUrls: ['user-image-control.component.scss']
+    styleUrls: ['user-image-control.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserImageControlComponent implements OnChanges {
+export class UserImageControlComponent implements OnChanges, AfterViewChecked, OnDestroy {
     @Input()
-    user: User;
-    @Input()
-    showProfileImageUploadButton: boolean;
+    profileImages: ProfileImageMetaInfo[] = [];
+
     @Output()
     profileImageUpload: EventEmitter<File> = new EventEmitter<File>();
     @Output()
@@ -40,56 +40,92 @@ export class UserImageControlComponent implements OnChanges {
         static: false
     }) fileInput: ElementRef;
 
-    public imageLoading: boolean;
-
-    public tiles: ImageTile[] = [];
+    @ViewChild(CdkVirtualScrollViewport, {
+        static: false
+    }) viewPort: CdkVirtualScrollViewport;
 
     private selectedIndex: number;
 
-    private totalLength: number;
-
-    public tiles$: BehaviorSubject<ImageTile[]> = new BehaviorSubject([]);
-
     private currentIndex: number;
 
+    private pageSize = 4;
+
+    private pages: number;
+
+    private currentPage = 0;
+
+    public imageLoading: boolean;
+
+    public totalLength: number;
+
+    public profileImages$: BehaviorSubject<ProfileImageMetaInfo[]> = new BehaviorSubject([]);
+
+    public isDevelopment = !environment.production;
+    private subscription: Subscription = null;
 
     public ngOnChanges(changes: SimpleChanges): void {
-        const userChange = changes['user'];
+        const profileImagesChange = changes['profileImages'];
         if (
-            !userChange ||
-            !this.user ||
-            !this.user.profileImageMetaInfos.length
+            !profileImagesChange ||
+            !this.profileImages ||
+            !this.profileImages.length
         ) {
-            this.tiles = [];
+            this.profileImages = [];
             return;
         }
 
-        if (userChange.previousValue && (<User>userChange.previousValue).profileImageMetaInfos.length === this.totalLength) {
-            return;
-        }
+        console.log(this.profileImages);
 
-        this.totalLength = this.user.profileImageMetaInfos.length;
+        this.totalLength = this.profileImages.length;
 
-        this.computeTiles(this.user.profileImageMetaInfos);
+        this.pages = Math.ceil(this.totalLength / this.pageSize);
+
+        console.log('PAGES', this.pages);
 
         this.viewPortScrolled(this.currentIndex);
+    }
 
-        const primaryImageIndex = this.user.profileImageMetaInfos
-            .findIndex(image => image.isPrimary);
+    public ngAfterViewChecked(): void {
+        if (this.viewPort && !this.subscription) {
+            this.subscription = this.viewPort.scrolledIndexChange.pipe(
+                delay(100)
+            ).subscribe(index => this.viewPortScrolled(index));
+        }
 
-        if (primaryImageIndex) {
-            this.setSelected(primaryImageIndex);
+        if (!this.viewPort && this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+    public ngOnDestroy(): void {
+        if (!this.subscription || this.subscription.closed) {
             return;
         }
 
-        this.setSelected(0);
+        this.subscription.unsubscribe();
+    }
+
+    public calculateColSpan(index: number): number {
+        console.log('CALC COLSPAN', index);
+
+        const indexPlusOne = index + 1;
+
+        if (indexPlusOne < this.totalLength) {
+            return 1;
+        }
+
+        if (indexPlusOne % 2 !== 0) {
+            return 2;
+        }
+
+        return 1;
     }
 
     public triggerFileDialog(): void {
         this.fileInput.nativeElement.click();
     }
 
-    public emitSelection($event): void {
+    public emitSelection($event: { target: { files: any; }; }): void {
         const files = [...$event.target.files];
 
         this.fileInput.nativeElement.value = null;
@@ -110,15 +146,15 @@ export class UserImageControlComponent implements OnChanges {
     }
 
     public requestProfileImageRemove(): void {
-        this.profileImageRemove.emit(this.user.profileImageMetaInfos[this.selectedIndex].removeUrl);
+        this.profileImageRemove.emit(this.profileImages[this.selectedIndex].removeUrl);
     }
 
     public requestMarkPrimaryProfileImage(): void {
-        this.profileImagePrimary.emit(this.user.profileImageMetaInfos[this.selectedIndex].id);
+        this.profileImagePrimary.emit(this.profileImages[this.selectedIndex].id);
     }
 
     public setSelected(index: number): void {
-        if (!this.tiles || !this.tiles.length) {
+        if (!this.profileImages || !this.profileImages.length) {
             return;
         }
 
@@ -126,59 +162,35 @@ export class UserImageControlComponent implements OnChanges {
     }
 
     public viewPortScrolled(index: number): void {
-        this.currentIndex = this.tiles$.getValue().length;
-        const plused = this.currentIndex + index;
-        const next = plused > 0 ? plused : 4;
-        const rounded = next % 2 === 0 ? next : (next + 1);
-        this.tiles$.next(this.tiles.slice(0, rounded));
+        console.log('NEW INDEX', index);
+        console.log('CURRENT INDEX', this.currentIndex);
+
+        const currentValue = this.profileImages$.getValue();
+        const currentLength = currentValue.length;
+
+        if (currentLength > this.totalLength) {
+            this.profileImages$.next(this.profileImages);
+            return;
+        }
+
+        if (currentLength === this.totalLength) {
+            this.currentIndex = index;
+            return;
+        }
+
+        const nonZeroIndex = (isNaN(index) ? 0 : index) + 1;
+        const nextItemCount = Math.max(nonZeroIndex, Math.max(this.pageSize, this.currentPage * this.pageSize));
+
+        const nextSliceEnd = Math.max(nextItemCount, this.currentPage * this.pageSize);
+
+        const nextSlice = this.profileImages.slice(currentLength, nextSliceEnd);
+        const nextValue = currentValue.concat(nextSlice);
+
+        this.currentPage++;
+        this.profileImages$.next(nextValue);
     }
 
-    private computeTiles(profileImageMetaInfos: ProfileImageMetaInfo[]): void {
-        if (!profileImageMetaInfos || !profileImageMetaInfos.length) {
-            return;
-        }
-
-        this.tiles.forEach((tile, tileIndex) => {
-            if (profileImageMetaInfos.findIndex(meta => meta.url === tile.url) === -1) {
-                this.tiles.splice(tileIndex, 1);
-            }
-        });
-
-        const factor = profileImageMetaInfos.length % 2 === 0 ? 1 : 2;
-
-        profileImageMetaInfos
-            .filter(meta => this.tiles.findIndex(tile => tile.url === meta.url) === -1)
-            .forEach((meta, index) => {
-                if (this.totalLength === 1) {
-                    this.tiles.push({
-                        url: meta.url,
-                        cols: factor,
-                        rows: 2,
-                    });
-                    return;
-                }
-
-                if (index < (this.totalLength - 1)) {
-                    this.tiles.push({
-                        url: meta.url,
-                        cols: 1,
-                        rows: 2,
-                    });
-                }
-
-                if (index === (this.totalLength - 1)) {
-                    this.tiles.push({
-                        url: meta.url,
-                        cols: factor,
-                        rows: factor,
-                    });
-                }
-            });
-
-        if (this.tiles.length % factor === 0 || this.tiles[this.tiles.length - 1].cols === factor) {
-            return;
-        }
-
-        this.tiles[this.tiles.length - 1].cols = factor;
+    public trackByFn(_index: number, meta: ProfileImageMetaInfo): string {
+        return meta.id;
     }
 }
