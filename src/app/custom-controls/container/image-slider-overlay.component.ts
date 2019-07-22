5cf23fd93b17db9e34099d46c9f1e4043b695a4c
alsami <al.khatib.sami1990@gmail.com>
2019-07-22 10:40:06 +0200

    feat: show loader state when selecting images via controls or thumbnails

--- src/app/custom-controls/container/image-slider-overlay.component.ts
+++ src/app/custom-controls/container/image-slider-overlay.component.ts
@@ -11,22 +11,26 @@ export class ImageSliderOverlayComponent {
     public selectedImageIndex: number;
     public selectedImageUrl: string;
     public windowWidth: number;
+    public imageLoadState: { url: string, loading: boolean }[] = [];
 
     public constructor(public dialogRef: ImageSliderOverlayRef,
         @Inject(ImageSliderOverlayToken) public imageSliderConfig: ImageSliderOverlayConfig) {
         this.selectedImageIndex = 0;
         this.selectedImageUrl = imageSliderConfig.thumbnails[0];
         this.windowWidth = window.innerWidth;
+        this.imageLoadState = this.createLoadState(imageSliderConfig);
     }
 
     public next(): void {
         if (this.selectedImageIndex === this.imageSliderConfig.thumbnails.length - 1) {
+            this.setSelectedImageLoading(0);
             this.selectedImageIndex = 0;
             this.selectedImageUrl = this.imageSliderConfig.thumbnails[0];
             return;
         }
 
         this.selectedImageIndex++;
+        this.setSelectedImageLoading(this.selectedImageIndex);
         this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
         return;
     }
@@ -34,17 +38,41 @@ export class ImageSliderOverlayComponent {
     public previous(): void {
         if (this.selectedImageIndex === 0) {
             this.selectedImageIndex = this.imageSliderConfig.thumbnails.length - 1;
+            this.setSelectedImageLoading(this.selectedImageIndex);
             this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
             return;
         }
 
         this.selectedImageIndex--;
+        this.setSelectedImageLoading(this.selectedImageIndex);
         this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
         return;
     }
 
     public selectImage(index: number): void {
+        this.setSelectedImageLoading(index);
         this.selectedImageIndex = index;
         this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
+    }
+
+    public selectedImageLoading(): boolean {
+        return this.imageLoadState[this.selectedImageIndex].loading;
+    }
+
+    public imageLoaded(): void {
+        this.imageLoadState[this.selectedImageIndex].loading = false;
+    }
+
+    private setSelectedImageLoading(index: number): void {
+        this.imageLoadState[index].loading = true;
+    }
+
+    private createLoadState(imageSliderConfig: ImageSliderOverlayConfig): { url: string, loading: boolean }[] {
+        return imageSliderConfig.thumbnails.map(url => {
+            return {
+                url: url,
+                loading: true
+            };
+        });
     }
 }
