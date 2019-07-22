8a94397f0427816bf915389a6bf62a1f1ea7ae3e
alsami <al.khatib.sami1990@gmail.com>
2019-07-22 10:20:07 +0200

    feat: optimize image slider and fix ui quirkies

--- src/app/custom-controls/container/image-slider-overlay.component.ts
+++ src/app/custom-controls/container/image-slider-overlay.component.ts
@@ -10,40 +10,41 @@ export class ImageSliderOverlayComponent {
 
     public selectedImageIndex: number;
     public selectedImageUrl: string;
-    public minBufferWidth: number;
-    public maxBufferWidth: number;
-    public viewPortWidth: number;
+    public windowWidth: number;
 
     public constructor(public dialogRef: ImageSliderOverlayRef,
         @Inject(ImageSliderOverlayToken) public imageSliderConfig: ImageSliderOverlayConfig) {
         this.selectedImageIndex = 0;
-        this.selectedImageUrl = imageSliderConfig.urls[0];
-        this.minBufferWidth = window.innerWidth / 4 * .8;
-        this.maxBufferWidth = this.minBufferWidth * 2;
-        this.viewPortWidth = window.innerWidth * .8;
+        this.selectedImageUrl = imageSliderConfig.thumbnails[0];
+        this.windowWidth = window.innerWidth;
     }
 
     public next(): void {
-        if (this.selectedImageIndex === this.imageSliderConfig.urls.length) {
+        if (this.selectedImageIndex === this.imageSliderConfig.thumbnails.length - 1) {
             this.selectedImageIndex = 0;
-            this.selectedImageUrl = this.imageSliderConfig.urls[0];
+            this.selectedImageUrl = this.imageSliderConfig.thumbnails[0];
             return;
         }
 
         this.selectedImageIndex++;
-        this.selectedImageUrl = this.imageSliderConfig.urls[this.selectedImageIndex];
+        this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
         return;
     }
 
     public previous(): void {
         if (this.selectedImageIndex === 0) {
-            this.selectedImageIndex = this.imageSliderConfig.urls.length - 1;
-            this.selectedImageUrl = this.imageSliderConfig.urls[this.selectedImageIndex];
+            this.selectedImageIndex = this.imageSliderConfig.thumbnails.length - 1;
+            this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
             return;
         }
 
         this.selectedImageIndex--;
-        this.selectedImageUrl = this.imageSliderConfig.urls[this.selectedImageIndex];
+        this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
         return;
+    }
+
+    public selectImage(index: number): void {
+        this.selectedImageIndex = index;
+        this.selectedImageUrl = this.imageSliderConfig.thumbnails[this.selectedImageIndex];
     }
 }
