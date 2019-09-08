c34cf0af23a4e9488a76b6c719637b80dcd7507c
alsami <al.khatib.sami1990@gmail.com>
2019-09-08 07:44:33 +0200

    feat: add signal-r client

--- src/app/browse/container/browse.component.ts
+++ src/app/browse/container/browse.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromRoot from '@etdb/+state';
 import { TitleActions } from '@etdb/core/+state/actions';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
 
 @Component({
     selector: 'etdb-browse',
@@ -10,8 +11,22 @@ import { TitleActions } from '@etdb/core/+state/actions';
 })
 export class BrowseComponent implements OnInit {
     public constructor(private store: Store<fromRoot.AppState>) { }
+    private hubConnection: HubConnection;
 
     public ngOnInit(): void {
+        this.hubConnection = new HubConnectionBuilder()
+            .withUrl('http://localhost:5001/hubs/chat')
+            .configureLogging(LogLevel.Critical)
+            .build();
+
+        this.hubConnection.start().then(() => {
+            this.hubConnection.on('ReceiveMessage', (user, message) => console.log('Received message', user, message));
+            this.hubConnection.invoke('SendMessage', 'some-user', 'some-message')
+                .then(() => console.log('SEND-MESSAGE'))
+                .catch(error => console.error('got error', error));
+        });
+
+
         this.store.dispatch(new TitleActions.SetTitle('Browse'));
     }
 }
