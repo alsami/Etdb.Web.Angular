8cca5ea3b741ba02bbc064a10629fe2420c00f7e
alsami <al.khatib.sami1990@gmail.com>
2019-09-21 08:09:59 +0200

    feat: move theme-menu into user-menu when user is authenticated

--- src/app/browse/container/browse.component.ts
+++ src/app/browse/container/browse.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromRoot from '@etdb/+state';
 import { TitleActions } from '@etdb/core/+state/actions';
-import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
+// import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
 
 @Component({
     selector: 'etdb-browse',
@@ -11,20 +11,21 @@ import { HubConnection, HubConnectionBuilder, LogLevel } from '@aspnet/signalr';
 })
 export class BrowseComponent implements OnInit {
     public constructor(private store: Store<fromRoot.AppState>) { }
-    private hubConnection: HubConnection;
+    // private hubConnection: HubConnection;
 
     public ngOnInit(): void {
-        this.hubConnection = new HubConnectionBuilder()
-            .withUrl('http://localhost:5001/hubs/chat')
-            .configureLogging(LogLevel.Critical)
-            .build();
+        //     this.hubConnection = new HubConnectionBuilder()
+        //     .withUrl('http://localhost:5001/hubs/chat')
+        //     .configureLogging(LogLevel.Critical)
+        //     .build();
 
-        this.hubConnection.start().then(() => {
-            this.hubConnection.on('ReceiveMessage', (user, message) => console.log('Received message', user, message));
-            this.hubConnection.invoke('SendMessage', 'some-user', 'some-message')
-                .then(() => console.log('SEND-MESSAGE'))
-                .catch(error => console.error('got error', error));
-        });
+        // this.hubConnection.start().then(() => {
+        //     this.hubConnection.on('ReceiveMessage', (user, message) => console.log('Received message', user, message));
+        //     this.hubConnection.invoke('SendMessage', 'some-user', 'some-message')
+        //         .then(() => console.log('SEND-MESSAGE'))
+        //         .catch(error => console.warn('got error', error));
+        // });
+
 
 
         this.store.dispatch(new TitleActions.SetTitle('Browse'));
